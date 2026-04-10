58acc98825192508ea19eb32905639a9364e8703
Dichha Rai <dichha.chamling@gmail.com>
2026-04-09 17:03:59 -0700

    decoding ascii art assessment

---
+++ decoding.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+
+def decode_message(url: str) -> any:
+    response = requests.get(url)
+    response.encoding = 'utf-8'
+
+    soup = BeautifulSoup(response.text, "html.parser")
+    rows = soup.find_all('tr')
+    print(soup.text[:500])
+
+    # print(f"Found {len(rows)} rows.\n") 
+    # print(f"1st row: {rows[0]}")
+
+    clean_lines = []
+    for _, row in enumerate(rows[1:]):  # skipping header
+        text = row.get_text()
+        parts = re.split(r'(\D+)', text)
+        clean_lines.append(parts)      
+
+    positions = []
+    i = 0
+
+    while i < len(clean_lines):
+        x = int(clean_lines[i][0])
+        char = clean_lines[i][1]
+        y = int(clean_lines[i][2])
+        positions.append((char, x, y))
+
+        i += 1
+
+    if not positions:
+        print("No valid data found.")
+        return
+    
+    # print(positions)
+    max_x = max(p[1] for p in positions)
+    max_y = max(p[2] for p in positions)
+    # print(max_x, max_y)
+
+    # empty string for grid without character
+    grid = [[' ' for _ in range(max_x+1)] for _ in range(max_y+1)]
+
+    for char, x, y in positions:
+        grid[max_y-y][x] = char
+    
+    for row in grid:
+        print(''.join(row))
+    
+
+decode_message("https://docs.google.com/document/d/e/2PACX-1vSvM5gDlNvt7npYHhp_XfsJvuntUhq184By5xO_pA4b_gCWeXb6dM6ZxwN8rE6S4ghUsCj2VKR21oEP/pub")
+# decode_message("https://docs.google.com/document/d/e/2PACX-1vTMOmshQe8YvaRXi6gEPKKlsC6UpFJSMAk4mQjLm_u1gmHdVVTaeh7nBNFBRlui0sTZ-snGwZM4DBCT/pub")
